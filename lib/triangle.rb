a69a8e98e756301a20510ac14781e94523ee28fe
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-26 13:41:26 -0700

    commit tree 84adb9dd0b5b3806c84f436de594402ca12fb4ad

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_reader :num1, :num2, :num3
+
+  def initialize(num1,num2,num3)
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
+  end
+
+  def kind
+    validate
+    if num1 == num2 && num2 == num3 
+      :equilateral
+    elsif num1 == num2 || num2 == num3 || num1 == num3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+ 
+  def no_negative_numbers
+    [num1, num2, num3].all?(&:positive?)
+   
+  end
+
+  def violate_inequality
+    num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1
+  end
+
+  def validate
+    raise TriangleError unless no_negative_numbers && violate_inequality
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+binding.pry
+
+
